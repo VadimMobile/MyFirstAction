c8896f338bcd1dbfab594d5822c8a24e0da5e1c7
Vadim Mobile <qwerty901cx@gmail.com>
2025-01-09 20:16:14 +0300

    Data storage

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
+import ru.netology.nmedia.repository.PostRepositoryFileImpl
 import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
 import ru.netology.nmedia.repository.PostRepositorySharedPrefsImpl
 
@@ -17,9 +18,9 @@ private val empty = Post(
     likedByMe = false
 )
 
-class PostViewModel() : ViewModel() {
+class PostViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository: PostRepository = PostRepositoryInMemoryImpl()
+    private val repository: PostRepository = PostRepositoryFileImpl(application)
 
     val data = repository.getAll()
 

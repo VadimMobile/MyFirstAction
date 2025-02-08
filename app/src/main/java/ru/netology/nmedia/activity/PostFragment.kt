3fd0716ee8cf9fe03c94fad4ee23bef4fea90542
Vadim Mobile <qwerty901cx@gmail.com>
2025-02-08 16:22:59 +0300

    Created a PostFragment class

---
+++ app/src/main/java/ru/netology/nmedia/activity/PostFragment.kt
@@ -0,0 +1,23 @@
+package ru.netology.nmedia.activity
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class PostFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val postId = arguments?.idArg ?: -1
+        viewModel.data.observe(viewLifecycleOwner) { posts ->
+            val post = posts.find { it.id == postId } ?: return@observe
+            with(binding) {
+                ...
+            }
+        }
+    }
+}
